bd0d2dd3692a473652623f4eee7f3c34136c8b2e
Stephen Price <stephen_lprice@live.com>
2021-02-01 10:28:22 -0600

    responding with JSON

--- server.js
+++ server.js
@@ -6,34 +6,35 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const reservations= [
+const reservations = [
     {
         routeName: 'reservation1',
         name: "Dan",
-        tableNumber: 7,
-
+        phone: 512123456,
+        email: "dan@bushong.com",
+        unique: "sdfd789234r"
     }
-    
-]
+];
 
 
-
+// Homepage
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'index.html')));
 
+// Tables
 app.get('/tables', (req,res) => res.sendFile(path.join(__dirname, 'tables.html')));
 
+// Reservation
 app.get('/reservation', (req,res) => res.sendFile(path.join(__dirname, 'reservation.html')));
 
+// API
 app.post('/api/reservation', (req,res) => {
     const newReservation = req.body;
 
     reservations.push(newReservation);
     res.json(newReservation);
-
-
 })
 
-app.get('/api/reservation/')
+app.get('/api/reservation/', (req,res) => res.json(reservations));
 
 app.listen(PORT, () => console.log(`app listening on PORT ${PORT}`));
 
